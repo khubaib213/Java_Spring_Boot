1866cbdcd13312ab0ac37f0be15d17bfc2e9b818
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-01 21:18:59 +0500

    Implemented Logger class for User.service & Authservice

--- myJar/src/main/java/com/example/myJar/AuthService.java
+++ myJar/src/main/java/com/example/myJar/AuthService.java
@@ -1,12 +1,16 @@
 package com.example.myJar;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+
+    public static final Logger logger = LoggerFactory.getLogger(AuthService.class);
     private final AuthUserRepository authUserRepository;
     private final JwtService jwtService;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -19,25 +23,34 @@ public class AuthService {
 
     public String register(RegisterDTO request)
     {
+        logger.info("Registering User with email:{}", request.getEmail());
         if(authUserRepository.findByEmail(request.getEmail()).isPresent())
         {
+            logger.warn("Registration failed - Email already exists:{}", request.getEmail());
             throw new RuntimeException("Email already exists");
         }
         String hashedPassword = passwordEncoder.encode(request.getPassword());
         AuthUsers user = new AuthUsers(request.getEmail(), hashedPassword, "Role_User");
         authUserRepository.save(user);
+
+        logger.info("User resgistered successfully:{}", request.getEmail());
         return jwtService.generateToken(request.getEmail());
     }
 
     public String login(LoginDTO request)
     {
-        AuthUsers user = authUserRepository.findByEmail(request.getEmail()).orElseThrow(()-> new ResourceNotFoundException("User not found"));
-
-
+        logger.info("Login attempt for the email:{}", request.getEmail());
+        AuthUsers user = authUserRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> {
+                    logger.error("Login failed — user not found: {}", request.getEmail());
+                    return new ResourceNotFoundException("User not found");
+                });
         if(!passwordEncoder.matches(request.getPassword(), user.getPassword()))
         {
+            logger.warn("Login attempt failed - Incorrect Password: {}",request.getPassword());
             throw new RuntimeException("User not found");
         }
+        logger.info("Login successful for: {}", request.getEmail());
         return jwtService.generateToken(request.getEmail());
     }
 }
--- myJar/src/main/java/com/example/myJar/UserService.java
+++ myJar/src/main/java/com/example/myJar/UserService.java
@@ -1,5 +1,7 @@
 package com.example.myJar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
 
@@ -29,30 +33,34 @@ public class UserService {
 
     public UserDTO createUser(CreateUserDTO request)
     {
+        logger.info("Creating User with email:{}", request.getEmail());
         User saved = new User(request.getName(), request.getEmail());
         User user = userRepository.save(saved);
+        logger.info("User created successfully with id: {}", user.getId());
         return ConvertToDTO(user);
     }
 
     public List<UserDTO> getAllUsers ()
     {
+        logger.info("Fetching all Users");
         return userRepository.findAll()
                 .stream()
                 .map(this::ConvertToDTO)
                 .toList();
     }
 
-    public UserDTO findById(int id)
-    {
-        User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User not found with id: "+id));
-        if (user ==  null)
-        {
-            return null;
-        }
+    public UserDTO findById(int id) {
+        logger.info("Fetching user with id: {}", id);
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.error("User not found with id: {}", id);
+                    return new ResourceNotFoundException("User not found with id: " + id);
+                });
         return ConvertToDTO(user);
     }
 
     public Page<UserDTO> getAllUsersPaginated(int page, int size, String sortBy) {
+        logger.info("Dteching all the User -- page:{}, size:{}, sortBy:{}", page, size, sortBy);
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
         return userRepository.findAll(pageable)
                 .map(this::ConvertToDTO);
@@ -60,6 +68,7 @@ public class UserService {
 
     public List<UserDTO> searchByName (String Keyword)
     {
+        logger.info("Searching Users by keyword :{}", Keyword);
         return userRepository.searchByName(Keyword)
                 .stream()
                 .map(this::ConvertToDTO)
